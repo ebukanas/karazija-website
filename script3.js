765fad924adc8dfa0e4d8c39285a36d986bc3c5d
ebukanas <edvinas.bukanas@gmail.com>
2022-06-09 21:55:23 +0300

    test
    .

--- script3.js
+++ script3.js
@@ -1,4 +1,4 @@
-// declare variables
+// DOM elements ----------
 const sections =  document.querySelectorAll('section');
 const footerTitle = document.querySelector('.center h1');
 const footerIndex = document.querySelector('.left');
@@ -10,42 +10,55 @@ const navbar = document.querySelector('.navbar');
 const sectionImages = document.querySelectorAll('.project-image');
 
 
-//functions
+// variables -----------
 
-// let options = {
-//     root: null,
-//     rootMargin: '0px',
-//     threshold: 1.0
-//   }
+// functions -----------
 
 const isTargetVisible = target => {
     //Intersection Observer API
   const io = new IntersectionObserver((entries) => {
-    console.log(entries)
     entries.forEach(entry => {
-
       if (entry.isIntersecting) {
         const img = entry.target;
-        var ID = img.parentElement.id;
-        var totalSections = sections.length;
-
-        if (ID === 'first-img') {
-            footerTitle.innerHTML = 'Betono gabalas';
-            footerIndex.innerHTML = `01/0${totalSections}`;
-            console.log(ID + ' changed');
-        } else if (ID === 'second-img') {
-            footerTitle.innerHTML = 'Chujnių gatvė';
-            footerIndex.innerHTML = `02/0${totalSections}`;
-        } else if (ID === 'third-img') {
-            footerTitle.innerHTML = 'Kažkoks kaimas';
-            footerIndex.innerHTML = `03/0${totalSections}`;
-        }
+        handleChanges(img);
       }
     });
   });
-
   io.observe(target)
 };
 
 //checks if an image is visible in the viewport
 sectionImages.forEach(isTargetVisible);
+
+function handleChanges(img) {
+    var ID = img.parentElement.id;
+    var totalSections = sections.length;
+
+    console.log(ID + ' is visible');
+
+    if (ID === 'first-img') {
+        footerTitle.innerHTML = 'Betono gabalas';
+        footerIndex.innerHTML = `01/0${totalSections}`;
+    } else if (ID === 'second-img') {
+        footerTitle.innerHTML = 'Chujnių gatvė';
+        footerIndex.innerHTML = `02/0${totalSections}`;
+    } else if (ID === 'third-img') {
+        footerTitle.innerHTML = 'Kažkoks kaimas';
+        footerIndex.innerHTML = `03/0${totalSections}`;
+    }
+}
+
+//navbar 
+openMenu.addEventListener('click', function() {
+    closeMenu.style.display = 'block';
+    openMenu.style.display = 'none';
+    minResMenu.style.display = 'block';
+    navbar.style.border = 'none';
+})
+
+closeMenu.addEventListener('click', function() {
+    closeMenu.style.display = 'none';
+    openMenu.style.display = 'block';
+    minResMenu.style.display = 'none';
+    navbar.style.border = '1px solid black';
+})
