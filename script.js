696e5a2df751f4bdc826082136f21bfeffc26918
ebukanas <edvinas.bukanas@gmail.com>
2022-06-15 16:09:24 +0300

    testing

--- script.js
+++ script.js
@@ -20,18 +20,21 @@ let vh = window.innerHeight * 0.01;
 // Then we set the value in the --vh custom property to the root of the document
 document.documentElement.style.setProperty('--vh', `${vh}px`);
 
-// We listen to the resize event
-window.addEventListener('resize', () => {
+function setHeight() {
   // We execute the same script as before
   let vh = window.innerHeight * 0.01;
   document.documentElement.style.setProperty('--vh', `${vh}px`);
-});
+}
+
+// We listen to the resize event
+window.addEventListener('resize', setHeight);
 
 const isTargetVisible = target => {
     //Intersection Observer API
   const io = new IntersectionObserver((entries) => {
     entries.forEach(entry => {
       if (entry.isIntersecting) {
+        setHeight();
         const img = entry.target;
         footerAnimation();
         handleChanges(img);
