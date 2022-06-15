dd3eb8b0c9ae307e63bda785137bb44d0cba02be
ebukanas <edvinas.bukanas@gmail.com>
2022-06-15 17:09:22 +0300

    testing

--- script.js
+++ script.js
@@ -18,13 +18,13 @@ const sectionImages = document.querySelectorAll('.project-image');
 // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
 let vh = window.innerHeight * 0.01;
 // Then we set the value in the --vh custom property to the root of the document
-// document.documentElement.style.setProperty('--vh', `${vh}px`);
+document.documentElement.style.setProperty('--vh', `${vh}px`);
 
 function setHeight() {
   // We execute the same script as before
   // console.log('running');
   let vh = window.innerHeight * 0.01;
-  // document.documentElement.style.setProperty('--height', `${vh}px`);
+  document.documentElement.style.setProperty('--height', `${vh}px`);
 }
 
 // We listen to the resize event
